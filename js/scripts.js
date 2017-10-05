98d9f8d3d6dca620aa66f6c52be312b2a229276e
rwart <32432397+rwart@users.noreply.github.com>
2017-10-05 18:18:27 +0200

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 // scripts.js
 
+/* name - a name of user */
+
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+// alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+/* This is multi line
+
+comment */
